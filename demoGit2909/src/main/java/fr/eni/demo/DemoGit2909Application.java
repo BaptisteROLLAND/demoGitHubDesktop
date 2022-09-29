464241793b04021d0e97401d66ef3579c8df250d
BaptisteROLLAND <baptiste.rolland2021@campus-eni.fr>
2022-09-29 15:07:40 +0200

    création du projet

---
+++ demoGit2909/src/main/java/fr/eni/demo/DemoGit2909Application.java
@@ -0,0 +1,13 @@
+package fr.eni.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit2909Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGit2909Application.class, args);
+	}
+
+}
