7d57f64d946ecea5d512fe29d2fd7a97cefc6e42
BaptisteROLLAND <baptiste.rolland2021@campus-eni.fr>
2022-09-30 16:36:03 +0200

    getListe

--- filmotheque/src/main/java/fr/eni/filmotheque/services/FilmServiceImpl.java
+++ filmotheque/src/main/java/fr/eni/filmotheque/services/FilmServiceImpl.java
@@ -1,17 +1,36 @@
 package fr.eni.filmotheque.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import fr.eni.filmotheque.bo.Film;
 
 @Service
-public class FilmServiceImpl {
+public class FilmServiceImpl implements FilmService{
 
+	private List<Film> listeFilms;
+	
+	public FilmServiceImpl() {
+		listeFilms = new ArrayList<>();
+//		Film film = new Film(1, "nom 1", 1991, 1, "synopsis 1");
+//		listeFilms.add(film);
+	}
+	
 	public Film getFilmById(int id) {
 		
-		Film film = new Film();
+		for(Film film: listeFilms) {
+			if (film.getId() == id) {
+				return film;
+			}
+		}
 		
-		return film;
+		return null;
+	}
+	
+	public List<Film> getListeFilms(){
+		return new ArrayList<Film>();
 	}
 
 }
